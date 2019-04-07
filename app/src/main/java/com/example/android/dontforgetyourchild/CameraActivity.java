497b49ae4c021387129fd555d5ed83d6296c071c
Piotr Rozponczyk <Piotr.Rozponczyk@harman.com>
2019-04-07 23:52:07 +0300

    add able to send SMS

--- app/src/main/java/com/example/android/dontforgetyourchild/CameraActivity.java
+++ app/src/main/java/com/example/android/dontforgetyourchild/CameraActivity.java
@@ -16,10 +16,17 @@ limitations under the License.
 package com.example.android.dontforgetyourchild;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.util.Log;
 
 /** Main {@code Activity} class for the Camera app. */
 public class CameraActivity extends Activity {
+  PowerConnectionReceiver myPowerReeiver = new PowerConnectionReceiver();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,31 @@ public class CameraActivity extends Activity {
           .replace(R.id.container, Camera2BasicFragment.newInstance())
           .commit();
     }
+
+    //battery
+    BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+      int scale = -1;
+      int level = -1;
+      int voltage = -1;
+      int temp = -1;
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+        voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+        if (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) > 0)
+        {
+          //Log.e("BatteryManager", "level is "+level+"/"+scale+", temp is "+temp+", voltage is "+voltage);
+        }
+        else
+        {
+          Log.e("notplugged", "NOTPLUGGED");
+        }
+      }
+    };
+    IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    registerReceiver(batteryReceiver, filter);
+
   }
 }
--- app/src/main/java/com/example/android/dontforgetyourchild/ImageClassifier.java
+++ app/src/main/java/com/example/android/dontforgetyourchild/ImageClassifier.java
@@ -19,6 +19,7 @@ import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.os.SystemClock;
+import android.telephony.SmsManager;
 import android.util.Log;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -28,6 +29,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.sql.Time;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -39,19 +41,32 @@ import org.tensorflow.lite.Interpreter;
 /** Classifies images with Tensorflow Lite. */
 public class ImageClassifier {
 
-  /** Tag for the {@link Log}. */
+  static long savedTime = System.currentTimeMillis();
+  static int tenTimesToAlert = 0;
+
+  /**
+   * Tag for the {@link Log}.
+   */
   private static final String TAG = "TfLiteCameraDemo";
 
-  /** Name of the model file stored in Assets. */
+  /**
+   * Name of the model file stored in Assets.
+   */
   private static final String MODEL_PATH = "graph.lite";
 
-  /** Name of the label file stored in Assets. */
+  /**
+   * Name of the label file stored in Assets.
+   */
   private static final String LABEL_PATH = "labels.txt";
 
-  /** Number of results to show in the UI. */
+  /**
+   * Number of results to show in the UI.
+   */
   private static final int RESULTS_TO_SHOW = 3;
 
-  /** Dimensions of inputs. */
+  /**
+   * Dimensions of inputs.
+   */
   private static final int DIM_BATCH_SIZE = 1;
 
   private static final int DIM_PIXEL_SIZE = 3;
@@ -66,46 +81,63 @@ public class ImageClassifier {
   /* Preallocated buffers for storing image data in. */
   private int[] intValues = new int[DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y];
 
-  /** An instance of the driver class to run model inference with Tensorflow Lite. */
+  /**
+   * An instance of the driver class to run model inference with Tensorflow Lite.
+   */
   private Interpreter tflite;
 
-  /** Labels corresponding to the output of the vision model. */
+  /**
+   * Labels corresponding to the output of the vision model.
+   */
   private List<String> labelList;
 
-  /** A ByteBuffer to hold image data, to be feed into Tensorflow Lite as inputs. */
+  /**
+   * A ByteBuffer to hold image data, to be feed into Tensorflow Lite as inputs.
+   */
   private ByteBuffer imgData = null;
 
-  /** An array to hold inference results, to be feed into Tensorflow Lite as outputs. */
+  /**
+   * An array to hold inference results, to be feed into Tensorflow Lite as outputs.
+   */
   private float[][] labelProbArray = null;
-  /** multi-stage low pass filter **/
+  /**
+   * multi-stage low pass filter
+   **/
   private float[][] filterLabelProbArray = null;
   private static final int FILTER_STAGES = 3;
   private static final float FILTER_FACTOR = 0.4f;
 
   private PriorityQueue<Map.Entry<String, Float>> sortedLabels =
-      new PriorityQueue<>(
-          RESULTS_TO_SHOW,
-          new Comparator<Map.Entry<String, Float>>() {
-            @Override
-            public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
-              return (o1.getValue()).compareTo(o2.getValue());
-            }
-          });
+          new PriorityQueue<>(
+                  RESULTS_TO_SHOW,
+                  new Comparator<Map.Entry<String, Float>>() {
+                    @Override
+                    public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+                      return (o1.getValue()).compareTo(o2.getValue());
+                    }
+                  });
 
-  /** Initializes an {@code ImageClassifier}. */
+
+  SmsManager smsManager = SmsManager.getDefault();
+
+  /**
+   * Initializes an {@code ImageClassifier}.
+   */
   ImageClassifier(Activity activity) throws IOException {
     tflite = new Interpreter(loadModelFile(activity));
     labelList = loadLabelList(activity);
     imgData =
-        ByteBuffer.allocateDirect(
-            4 * DIM_BATCH_SIZE * DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y * DIM_PIXEL_SIZE);
+            ByteBuffer.allocateDirect(
+                    4 * DIM_BATCH_SIZE * DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y * DIM_PIXEL_SIZE);
     imgData.order(ByteOrder.nativeOrder());
     labelProbArray = new float[1][labelList.size()];
     filterLabelProbArray = new float[FILTER_STAGES][labelList.size()];
     Log.d(TAG, "Created a Tensorflow Lite Image Classifier.");
   }
 
-  /** Classifies a frame from the preview stream. */
+  /**
+   * Classifies a frame from the preview stream.
+   */
   String classifyFrame(Bitmap bitmap) {
     if (tflite == null) {
       Log.e(TAG, "Image classifier has not been initialized; Skipped.");
@@ -127,41 +159,45 @@ public class ImageClassifier {
     return textToShow;
   }
 
-  void applyFilter(){
-    int num_labels =  labelList.size();
+  void applyFilter() {
+    int num_labels = labelList.size();
 
     // Low pass filter `labelProbArray` into the first stage of the filter.
-    for(int j=0; j<num_labels; ++j){
-      filterLabelProbArray[0][j] += FILTER_FACTOR*(labelProbArray[0][j] -
-                                                   filterLabelProbArray[0][j]);
+    for (int j = 0; j < num_labels; ++j) {
+      filterLabelProbArray[0][j] += FILTER_FACTOR * (labelProbArray[0][j] -
+              filterLabelProbArray[0][j]);
     }
     // Low pass filter each stage into the next.
-    for (int i=1; i<FILTER_STAGES; ++i){
-      for(int j=0; j<num_labels; ++j){
-        filterLabelProbArray[i][j] += FILTER_FACTOR*(
-                filterLabelProbArray[i-1][j] -
-                filterLabelProbArray[i][j]);
+    for (int i = 1; i < FILTER_STAGES; ++i) {
+      for (int j = 0; j < num_labels; ++j) {
+        filterLabelProbArray[i][j] += FILTER_FACTOR * (
+                filterLabelProbArray[i - 1][j] -
+                        filterLabelProbArray[i][j]);
 
       }
     }
 
     // Copy the last stage filter output back to `labelProbArray`.
-    for(int j=0; j<num_labels; ++j){
-      labelProbArray[0][j] = filterLabelProbArray[FILTER_STAGES-1][j];
+    for (int j = 0; j < num_labels; ++j) {
+      labelProbArray[0][j] = filterLabelProbArray[FILTER_STAGES - 1][j];
     }
   }
 
-  /** Closes tflite to release resources. */
+  /**
+   * Closes tflite to release resources.
+   */
   public void close() {
     tflite.close();
     tflite = null;
   }
 
-  /** Reads label list from Assets. */
+  /**
+   * Reads label list from Assets.
+   */
   private List<String> loadLabelList(Activity activity) throws IOException {
     List<String> labelList = new ArrayList<String>();
     BufferedReader reader =
-        new BufferedReader(new InputStreamReader(activity.getAssets().open(LABEL_PATH)));
+            new BufferedReader(new InputStreamReader(activity.getAssets().open(LABEL_PATH)));
     String line;
     while ((line = reader.readLine()) != null) {
       labelList.add(line);
@@ -170,7 +206,9 @@ public class ImageClassifier {
     return labelList;
   }
 
-  /** Memory-map the model file in Assets. */
+  /**
+   * Memory-map the model file in Assets.
+   */
   private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
     AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_PATH);
     FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
@@ -180,7 +218,9 @@ public class ImageClassifier {
     return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
   }
 
-  /** Writes Image data into a {@code ByteBuffer}. */
+  /**
+   * Writes Image data into a {@code ByteBuffer}.
+   */
   private void convertBitmapToByteBuffer(Bitmap bitmap) {
     if (imgData == null) {
       return;
@@ -193,30 +233,64 @@ public class ImageClassifier {
     for (int i = 0; i < DIM_IMG_SIZE_X; ++i) {
       for (int j = 0; j < DIM_IMG_SIZE_Y; ++j) {
         final int val = intValues[pixel++];
-        imgData.putFloat((((val >> 16) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
-        imgData.putFloat((((val >> 8) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
-        imgData.putFloat((((val) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+        imgData.putFloat((((val >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+        imgData.putFloat((((val >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+        imgData.putFloat((((val) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
       }
     }
     long endTime = SystemClock.uptimeMillis();
     Log.d(TAG, "Timecost to put values into ByteBuffer: " + Long.toString(endTime - startTime));
   }
 
-  /** Prints top-K labels, to be shown in UI as the results. */
+  /**
+   * Prints top-K labels, to be shown in UI as the results.
+   */
   private String printTopKLabels() {
     for (int i = 0; i < labelList.size(); ++i) {
       sortedLabels.add(
-          new AbstractMap.SimpleEntry<>(labelList.get(i), labelProbArray[0][i]));
+              new AbstractMap.SimpleEntry<>(labelList.get(i), labelProbArray[0][i]));
       if (sortedLabels.size() > RESULTS_TO_SHOW) {
         sortedLabels.poll();
       }
     }
     String textToShow = "";
+
+
     final int size = sortedLabels.size();
+    Log.d(TAG, "size: " + size);
     for (int i = 0; i < size; ++i) {
+      long currentTime = System.currentTimeMillis();
       Map.Entry<String, Float> label = sortedLabels.poll();
-      textToShow = String.format("\n%s: %4.2f",label.getKey(),label.getValue()) + textToShow;
-    }
+      textToShow = String.format("\n%s: %4.2f", label.getKey(), label.getValue()) + textToShow;
+
+        Log.d(TAG, "tenTimesToAlert: " + tenTimesToAlert);
+        Log.d(TAG, "savedTime: " + savedTime);
+        Log.d(TAG, "textToShow: " + textToShow);
+
+        if (label.getKey().contains("alert") && label.getValue() >= 0.9)
+        {
+          if (3000 <= (currentTime - savedTime))
+          {
+            Log.d(TAG, "tenTimesToAlert: ++");
+            tenTimesToAlert++;
+            savedTime = currentTime;
+          }
+        }
+        else if (label.getKey().contains("alert"))
+        {
+          Log.d(TAG, "tenTimesToAlert: 00");
+          tenTimesToAlert=0;
+        }
+
+        if (tenTimesToAlert>=9)
+        {
+          // TODO -> send sms
+          //smsManager.sendTextMessage("00972542200912", null, "DON'T FORGET YOUR CHILD!", null, null);
+          //smsManager.sendTextMessage("00972525515875", null, "sms message: ALERT!", null, null);
+          Log.d(TAG, "SMS: DON'T FORGET YOUR CHILD!");
+          tenTimesToAlert = 0;
+        }
+      }
     return textToShow;
   }
 }
